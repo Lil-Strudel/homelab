a2c9c1f9641c80f75a4facf31d50284f0668aa86
Lil-Strudel <42657503+Lil-Strudel@users.noreply.github.com>
2025-11-07 15:10:32 -0700

    refactor: rename mai to makima

--- talos/gen-machine-configs.sh
+++ talos/gen-machine-configs.sh
@@ -1,3 +1,4 @@
+mkdir -p machine-patches
 talosctl machineconfig patch talos/controlplane.yaml --patch @machine-patches/mai-1-patch.yaml --output machine-configs/mai-1.yaml
 talosctl machineconfig patch talos/controlplane.yaml --patch @machine-patches/mai-2-patch.yaml --output machine-configs/mai-2.yaml
 talosctl machineconfig patch talos/controlplane.yaml --patch @machine-patches/mai-3-patch.yaml --output machine-configs/mai-3.yaml
--- talos/gen-talos-objects.sh
+++ talos/gen-talos-objects.sh
@@ -1,2 +1,3 @@
+mkdir -p talos
 talosctl gen secrets -o talos/secrets.yaml
 talosctl gen config --with-secrets talos/secrets.yaml strudelnetes https://10.69.60.10:6443 --config-patch @patch.yaml --output ./talos --force
